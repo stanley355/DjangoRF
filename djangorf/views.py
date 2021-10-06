f8746ba37d5c41df9f804ed3761ce8a7262c5a6c
stanley355 <winatastanley355@gmail.com>
2021-10-06 08:23:37 +0700

    API view

--- djangorf/views.py
+++ djangorf/views.py
@@ -1,2 +1,13 @@
+from django.http import response
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'username': 'admin',
+            'years_active': 10
+        }
+        return Response(data)
