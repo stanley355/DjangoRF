dd1a8936a6d5ace4592e23b49f3a3067f62eca83
stanley355 <winatastanley355@gmail.com>
2021-10-07 19:51:48 +0700

    Startapp

--- djangorf/views.py
+++ djangorf/views.py
@@ -2,7 +2,6 @@ from django.http import response
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.response import Response
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
