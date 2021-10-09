9a8c458795c21c502230c4e88fe2e158e2040447
stanley355 <winatastanley355@gmail.com>
2021-10-09 14:38:44 +0700

    Rest framework get

--- djangorf/views.py
+++ djangorf/views.py
@@ -3,17 +3,22 @@ from django.http import response
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from drfapp.serializers import StudentSerializer
 from drfapp.models import Student
 
 class TestView(APIView):
+    permission_classes = (IsAuthenticated)
+
     def get(self, request, *args, **kwargs):
-        data = {
-            'username': 'admin',
-            'years_active': 10
-        }
-        return Response(data)
+        # For all
+        qs = Student.objects.all()
+        serializer = StudentSerializer(qs, many = True)
+        # For one object
+        # student1 = qs.first()
+        # serializer = StudentSerializer(student1)
+        return Response(serializer.data)
     
     def post(self, request, *args, **kwargs):
         serializer = StudentSerializer(data=request.data)
